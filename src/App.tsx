055a1889176b3f9b0dcb197bbfabc94f0c7af38e
Enock <bernardo.enock@vileve.com.br>
2025-04-22 15:54:44 -0300

    feat: Create a dinamic router

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
-function App() {
-  
+import { Routes, Route, Link } from 'react-router-dom';
+import ApiPage from './pages/ApiPage';
+import CsvPage from './pages/CsvPage';
 
+function App() {
   return (
-    <>
-      <h1>CSV API</h1>
-    </>
-  )
+    <div>
+      <nav style={{ marginBottom: '20px' }}>
+        <Link to="/" style={{ marginRight: '10px' }}>Fetch Users</Link>
+        <Link to="/csv">CSV Manager</Link>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<ApiPage />} />
+        <Route path="/csv" element={<CsvPage />} />
+      </Routes>
+    </div>
+  );
 }
 
-export default App
+export default App;
